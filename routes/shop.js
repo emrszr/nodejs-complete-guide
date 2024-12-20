49848b78302ff4bdf773d7faacb838ac42706c9e
emrszr <emerszr@gmail.com>
2024-12-20 23:30:47 +0300

    handlebars template

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,23 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
-const adminData = require('./admin');
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCss: true,
+  });
 });
 
 module.exports = router;
